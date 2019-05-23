fb3a72364eaf2580f91e3448a9147e2e1f95f006
enzoamorim1999 <enzoamorim1999@gmail.com>
2019-05-22 21:58:17 -0300

    model usuario, e DAO Usuario

--- src/main/java/com/poo/davi/enzo/salaobeleza/DAO/UsuarioDAO.java
+++ src/main/java/com/poo/davi/enzo/salaobeleza/DAO/UsuarioDAO.java
@@ -5,10 +5,53 @@
  */
 package com.poo.davi.enzo.salaobeleza.DAO;
 
+import com.poo.davi.enzo.salaobeleza.Connection.DbManager;
+import com.poo.davi.enzo.salaobeleza.Model.Usuario;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
- * @author Casa
+ * @author Enzo Amorim
  */
 public class UsuarioDAO {
-    
+
+    public static boolean salvar(Usuario usuario) {
+        try {
+
+            Connection conexao = DbManager.conectaDB();
+            PreparedStatement comando = conexao.prepareStatement("INSERT INTO "
+                    + "`usuarios`" + "("
+                    + "`nome`,"
+                    + "`cargo`,"
+                    + "`login`,"
+                    + "`senha`,"
+                    + "`excluido`)"
+                    + "VALUES"
+                    + "(?,"
+                    + "?,"
+                    + "?,"
+                    + "?,"
+                    +"0");
+            
+
+            comando.setString(1, usuario.getNome());
+            comando.setString(2, usuario.getCargo());
+            comando.setString(3, usuario.getLogin());
+            comando.setString(4, usuario.getSenha());
+            
+            
+            System.out.println(comando);
+
+            int linhasAfetadas = comando.executeUpdate();
+            return true;
+        } catch (SQLException ex) {
+            Logger.getLogger(UsuarioDAO.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+    }
+
 }
--- src/main/java/com/poo/davi/enzo/salaobeleza/Model/Usuario.java
+++ src/main/java/com/poo/davi/enzo/salaobeleza/Model/Usuario.java
@@ -10,5 +10,50 @@ package com.poo.davi.enzo.salaobeleza.Model;
  * @author Casa
  */
 public class Usuario {
-    
+
+    private String nome;
+    private String cargo;
+    private String login;
+    private String senha;
+
+    public Usuario(String nome, String cargo, String login, String senha) {
+
+        this.nome = nome;
+        this.cargo = cargo;
+        this.login = login;
+        this.senha = senha;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
 }
