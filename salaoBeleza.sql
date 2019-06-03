30c7458f0d4e1a8112fdd5c0bcd922a7ac835036
enzoamorim1999 <enzoamorim1999@gmail.com>
2019-06-03 19:49:40 -0300

    script

---
+++ salaoBeleza.sql
@@ -0,0 +1,20 @@
+CREATE TABLE usuarios ( 
+id int NOT NULL Primary key AUTO_INCREMENT,
+ nome VARCHAR(30) NOT NULL,
+ cpf int(11) NOT NULL,
+ telefone int(11),
+ cidade VARCHAR(20),
+ estado varchar(2) );
+ 
+ CREATE TABLE servicos ( 
+ id int NOT NULL Primary key AUTO_INCREMENT,
+ nome VARCHAR(30) NOT NULL,
+ tipoServico VARCHAR(30) NOT NULL
+ );
+
+INSERT INTO `servicos`(`nome`,`preco`) VALUES ('teste'10)
+
+INSERT INTO `usuarios`(`nome`,`cpf`,`telefone`,`cidade`,`estado`)VALUES('enzo',123123,123123,null,null)
+select * from servicos
+SELECT * FROM usuarios
+DELETE FROM usuarios WHERE id = 4
